125e5e98faa4e829bbf58a1c450f8bdb392711a1
SHArtyom <sartiom1@gmail.com>
2023-08-12 11:31:12 +0300

    Switched LANGUAGE_CODE default to 'ru-ru'

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -131,7 +131,7 @@ AUTH_USER_MODEL = 'users.User'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 LANGUAGES = (
     ('en-us', 'English'),
