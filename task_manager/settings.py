69ace2ee826434ce61e8c21b8702a3ab62d9117f
SHArtyom <sartiom1@gmail.com>
2023-08-12 11:06:10 +0300

    Minor update, settings.py adjusted for Render

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,12 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['webserver', '.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -95,8 +100,8 @@ if DEBUG is True:
 else:
     DATABASES = {
         'default': dj_database_url.config(
-            default='sqlite:///db.sqlite3',
-            conn_max_age=600,
+            default='postgresql://postgres:postgres@localhost:5432/task_manager',
+            conn_max_age=600
         )
     }
 
